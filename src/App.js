6188a06554ff522b182ac2116e4731135bb98897
Amany Mohamed <amany.ahmed1@vodafone.com>
2026-05-14 12:12:46 +0300

    add text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Amany </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
