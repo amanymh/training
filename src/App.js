05290ae09c0b26d8810851582d007e84b57a7f2f
Amany Mohamed <amany.ahmed1@vodafone.com>
2026-08-05 13:53:30 +0300

    add text to feature branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Amany </h1>
+        <p>this is a feature branch :)</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
